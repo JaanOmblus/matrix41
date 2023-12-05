1b099a1d4ea9c09cc7b9a16bc5d86fb616595a39
kvartiil <138615442+kvartiil@users.noreply.github.com>
2023-12-05 14:48:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# matrix41
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/kvartiil/matrix41)
---
+++ src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  //production: false,
+  //myCustomProperty: 'original value',
+  modelStruktuur: 0.9,
+  modelMaksevoime: 0.8,
+  modelRent: 0.2,
+  modelEfekt: 0.8,
+  modelLaen: 0.7
+};
+
+export const myNewConstant = {
+  key: 'value'
+};
